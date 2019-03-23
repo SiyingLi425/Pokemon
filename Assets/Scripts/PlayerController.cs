bd965a7b8001686c39dd21c516f57d9e24f42baa
Siying Li <yukimashiro425@gmail.com>
2019-03-22 20:27:12 -0400

    fixed Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     // Public Variables
     public float speed = 10.0f;
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
-
-    public GameObject laser;
-    public Transform laserSpawn;
-    public float fireRate = 0.5f;
+    public Animator animator;
 
     // Private Variables
     private Rigidbody2D rBody;
-    private float counter = 0.0f;
+ 
      
     // Start is called before the first frame update
     // Use this for initialization
@@ -29,15 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        counter += Time.deltaTime;
-        // Debug.Log(counter);
-
-        if (Input.GetButton("Fire1") && counter > fireRate)
-        {
-            // Create my laser object
-            Instantiate(laser, laserSpawn.position, laser.transform.rotation);
-            counter = 0.0f;
-        }
+        
     }
 
     void FixedUpdate()
@@ -51,9 +36,8 @@ public class PlayerController : MonoBehaviour
 
         rBody.velocity = newVelocity * speed;
 
-        // Restrict the player from leaving the play area
-        rBody.position = new Vector2(
-            Mathf.Clamp(rBody.position.x, minX, maxX),   // Restrict x position to -8 and 4
-            Mathf.Clamp(rBody.position.y, minY, maxY));  // Restrict y position to -4 and 4
+        animator.SetFloat("direction x", newVelocity.x);
+        animator.SetFloat("direction y", newVelocity.y);
+
     }
 }
