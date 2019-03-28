f08c4d0b766d59b8b18c84e83d4ec3f2855d3f95
Siying Li <yukimashiro425@gmail.com>
2019-03-28 13:21:53 -0400

    Added Dialogue system and wrapping
    two NPC with dialogues are added and wrapping to Poke-Center and back is possible

---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour {
+
+    public string level;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log("Collision");
+
+       if (other.tag == "Player")
+        {
+            Application.LoadLevel(level);
+
+            Debug.Log("this works");
+            
+        } 
+    }
+
+}
